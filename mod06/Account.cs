58cfc075ce17adef61f473b84731efff5dda519f
ГерценЕ <@403_TEACHER>
2017-06-30 22:09:56 +0600

    Add project files.

---
+++ mod06/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Bankomat
+{
+
+    namespace NAccount
+    {
+        class Account
+        {
+            string accNumber;
+            public string pass;
+            double amount;
+            Random rnum = new Random();
+
+            public Account ()
+            {
+                accNumber = rnum.Next(1, 100).ToString();
+                pass = "pass" + rnum.Next(1, 100).ToString();
+            }
+            //public string CreateAccNumber()
+            //{
+            //    Random rnum = new Random();
+            //    accNumber = rnum.Next(1, 100).ToString();
+            //    return accNumber;
+            //}
+            //public string CreatePass()
+            //{
+            //    Random rnum = new Random();
+            //    pass="pass"+ rnum.Next(1, 100).ToString();
+            //    return pass;
+            //}
+            public void putCash(double c)
+            {
+                amount = amount+c;
+            }
+            public void showPass()
+            {
+               Console.WriteLine(pass);
+            }
+            public void getAmount()
+            {
+                Console.WriteLine(amount);
+            }
+        }
+    }
+}
---
+++ mod06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NAccount.Account A1 = new NAccount.Account();
+            A1.showPass();
+
+            Console.WriteLine("Введите пароль карточки");
+            string carPass = Console.ReadLine();
+            if (carPass==A1.pass)
+            {
+                Console.WriteLine("a. Вывод баланса");
+                Console.WriteLine("b. Пополнение счёта");
+                Console.WriteLine("c. Снять деньги");
+                Console.WriteLine("d. Выход");
+                var c = Console.ReadLine();
+                while (true)
+                {
+                    switch (c)
+                    {
+                        case "a":
+                            A1.getAmount();
+                            Console.WriteLine("1. Вернутьсяв меню");
+                            Console.WriteLine("2. Выход");
+                            var a = Console.ReadLine();
+                            switch (a)
+                            {
+                                case "1":
+                                    break;
+                                case "2":
+                                    Console.ReadKey();
+                                    break;
+
+                            }
+                            break;
+                        case "b":
+                            Console.Write("Сколько положите?: ");
+                            A1.putCash(Double.Parse(Console.ReadLine()));
+                            break;
+                            //case "c":
+
+                    }
+                }
+                
+            }
+        }
+    }
+}
